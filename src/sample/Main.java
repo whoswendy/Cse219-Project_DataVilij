2daddf334b1a470740fccd3013f1df1fe1aa30b0
wendhu <wendhu@cs.stonybrook.edu>
2018-01-30 14:35:44 -0500

    first commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+        System.out.println("Hello World");
     }
 }
